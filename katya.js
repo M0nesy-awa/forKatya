0e6ea18e685c38e79dc2d79cc2aeef3e60cc2dbd
M0nesy-awa <haishun194@gmail.com>
2025-04-19 02:19:37 +0300

    Add files via upload

---
+++ katya.js
@@ -0,0 +1,21 @@
+document.addEventListener('click', function (event) {
+    const x = event.clientX / window.innerWidth;
+    const y = event.clientY / window.innerHeight;
+  
+    confetti({
+      particleCount: 100,
+      spread: 70,
+      origin: { x: x, y: y }
+    });
+  });
+  window.addEventListener("load", function () {
+    function updateHiddenMessage() {
+      let zoom = window.outerWidth / window.innerWidth;
+      let opacity = Math.min((zoom - 1) * 2, 1);
+      document.getElementById("hidden-message").style.opacity = opacity;
+    }
+  
+    updateHiddenMessage();
+    window.addEventListener("resize", updateHiddenMessage);
+  });
+  
